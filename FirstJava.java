a0bb04ed6d05d174a22057a3dfe7072291b30ec7
Sergiu Chilat <chilatsergiu@gmail.com>
2020-08-12 19:49:04 +0300

    Add Aritmetica class

--- FirstJava.java
+++ FirstJava.java
@@ -1,5 +1,13 @@
 class FirstJava{
 	public static void main(String [] args){
+		Aritmetica a = new Aritmetica();
 		System.out.println("First Java");
+		System.out.println(a.sum(2, 3));
+	}
+}
+
+class Aritmetica{
+	public int sum(int a, int b) {
+		return a + b;
 	}
 }
